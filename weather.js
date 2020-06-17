1a210506eac28ad5957c7f2c8fefdd838320dc2f
HelloKowshik <hellokowshik@gmail.com>
2020-06-18 02:32:51 +0600

    https problem solved

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ class Weather{
         this.API_KEY = 'c81e53b81606a1b882f5e34943cf73eb';
     }
     async getWeather() {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.API_KEY}&units=metric`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.API_KEY}&units=metric`);
         const responseData = await response.json();
         return {
             main_data: responseData.main,
